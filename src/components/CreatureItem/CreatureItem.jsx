2a427aba24f4bcfe8da6f545c211ed81afe0f637
Peder <116122618+pedergoodman@users.noreply.github.com>
2024-04-14 11:34:43 -0500

    finial edits

--- src/components/CreatureItem/CreatureItem.jsx
+++ src/components/CreatureItem/CreatureItem.jsx
@@ -1,7 +1,49 @@
+import { useState } from "react";
+
 function CreatureItem({ creature }) {
+
+  const [showStar, setShowStar] = useState(false)
+
+
+  const handleStar = () => {
+    console.log('clicked!');
+
+    setShowStar(!showStar)
+  }
+
+  let emoji = ''
+  if (showStar) {
+    emoji = '⭐️';
+  } else if (creature.origin === 'USA') {
+    emoji = '🇺🇸';
+  } else {
+    emoji = '💔';
+  }
+
+
+
+  console.log(creature.name, showStar);
   return (
-    <li>{creature.name} is from {creature.origin}</li>
+    <li
+      className={creature.origin === "scotland" && "green"}
+    >
+      {creature.name} is from {creature.origin}
+    <button
+      onClick={handleStar}
+    >STAR!</button>
+    {emoji}
+    {/* {showStar ? '⭐️' : '💔'} */}
+
+    </li>
   );
 };
 
 export default CreatureItem;
+
+
+
+
+// other options for className
+//turnery operator
+// className={creature.origin === "scotland" ? "green" : "purple"}
+
